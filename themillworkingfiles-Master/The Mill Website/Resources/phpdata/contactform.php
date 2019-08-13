fa2714a80b6b9729f7eef4cd6d80618c5c8c6014
aje-cox <ajecmultimedia@gmail.com>
2019-08-13 13:46:58 +1000

    Tweeked radio btns and contact form and php, added in photo for contact landing. To-do; txt, imgs

--- themillworkingfiles-Master/The Mill Website/Resources/phpdata/contactform.php
+++ themillworkingfiles-Master/The Mill Website/Resources/phpdata/contactform.php
@@ -1,3 +1,5 @@
+<!-- PLEASE NOTE; WHEN WEBSITE IS HOSTED, THE FOLLOWING PHP CODE WILL SEND ALL ENQUIRIES TO THE SPECIFIED BUSINESS EMAIL ADDRESS AND THUS THE CODE WILL NOT BE VISIBLE -->
+
 <?php
 
 if (isset($_POST['submit'])) {
@@ -8,11 +10,14 @@ if (isset($_POST['submit'])) {
   $state = $_POST['state'];
   $message = $_POST['message'];
 
-  $mailTo = "millwholefoods@gmail.com"; //fix
+  $mailTo = "millwholefoods@gmail.com"; //Server workaround for @gmail adresses implemented
   $headers = "Contact Enquiry From: ".$mailFrom;
   $txt = "You have recieved an enquiry from ".$name.", ".$country.", ".$state;
 
-  mail();
+  mail($mailTo, $headers, $txt);
+  header("Location: contactform.php?mailsend");
 }
 
 ?>
+
+<!-- TO RETURN TO THE WEBSITE, PLEASE CLICK THE BACK ARROW IN GOOGLE CHROME  -->
